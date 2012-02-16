a7a94afc1b8a147696831449465ec52944bffbf7
deepanshu.abroal <deepanshu.abroal@cbf59e0f-a343-4d94-a541-e188694bd9f4>
2012-02-16 07:48:11 +0000

    index.php changed
    git-svn-id: file:///labs/cse21/svn/Cse21_repo@14 cbf59e0f-a343-4d94-a541-e188694bd9f4

--- src/lab/index.php
+++ src/lab/index.php
@@ -43,8 +43,8 @@ $html = file_get_html('./content.html');
 
 $vlab_url = "http://virtual-labs.ac.in/"; 
 $css_js   = "http://deploy.virtual-labs.ac.in/labs/cse21/"; 
-$lab_url  = "http://deploy.virtual-labs.ac.in/labs/cse21/src/lab/index.php";
-$exp_url  = "http://deploy.virtual-labs.ac.in/labs/cse21/src/lab/exp/index.php";
+$lab_url  = "http://deploy.virtual-labs.ac.in/labs/cse21/index.php";
+$exp_url  = "http://deploy.virtual-labs.ac.in/labs/cse21/exp/index.php";
 $base_url = $lab_url;
 
 include('./exp_template.php');
